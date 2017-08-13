8c64986725f90382f761599fe86cc7dfb0a0ec79
kehylaicaza <keilacicaza@gmail.com>
2017-08-12 23:33:47 -0500

    ya no quiero

--- js/ver.js
+++ js/ver.js
@@ -1,47 +1,79 @@
-var a =0;
-var cuentosFinales=[];     
-
-
-
-$(document).ready(function () {
-    cuentosPorDefault();
-    imagenDefault();
-    imagenAgg();
-    
-    //slider
-$('body').on('click', '#principal a', function(){
-    //alert($(this).attr('id'));
-   a=$(this).attr('id');
-    obj=cuentosFinales[a]
-    for (let imagenes of obj.imagenes)
-    console.log(a);
-         $(".carousel-inner").append('<div class="item"><img src=' + imagenes + '</a></div>');
-  })
-
-
-});
+var a = 0;
+var cuentosFinales = [];
 
 function imagenAgg() {
     var aux = getCuentosLS();
-   
+
     for (let value of aux) {
- cuentosFinales.push(value);
-        $("#principal").append('<li><a id='+ a.toString()+ ' href="../paginas/cuento.html"><div class="col-lg-4"><img class="imagenesCuentos"  src=' + value.imagenes[0] + '>' + value.titulo + '</div></a></li>');
-a++;
+        cuentosFinales.push(value);
+        $("#principal").append('<li><a id=' + a.toString() + ' href="../paginas/cuento.html"><div class="col-lg-4"><img class="imagenesCuentos"  src=' + value.imagenes[0] + '>' + value.titulo + '</div></a></li>');
+        a++;
     }
 
 
 }
 
 function imagenDefault() {
- 
+
     for (let value of cuentos) {
-       cuentosFinales.push(value);
-        $("#principal").append('<li><a id='+ a.toString()+ '  href="../paginas/cuento.html"><div class="col-lg-4"><img class="imagenesCuentos"  src=' + value.imagenes[0] + '>' + value.titulo + '</div></a></li>');
-a++;
+        cuentosFinales.push(value);
+        $("#principal").append('<li><a id=' + a.toString() + '><div class="col-lg-4"><img alt=" " class="imagenesCuentos"  src=' + value.imagenes[0] + '>' + value.titulo + '</div></a></li>');
+        a++;
     }
 
 
 }
 
+$(document).ready(function () {
+    cuentosPorDefault();
+    imagenDefault();
+    imagenAgg();
 
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+        }
+    }
+
+
+    $('body').on('click', '#principal a', function () {
+        modal.style.display = "block";
+        a = $(this).attr('id');
+        mostrarCuento(a);
+
+
+    });
+});
+
+
+function mostrarCuento(v) {
+   
+    $("#nombreCuento").text(cuentosFinales[v].titulo);
+    $("#descripcion").text(cuentosFinales[v].descripcion);
+    $("#creditos").text(cuentosFinales[v].creditos);
+    var n=1;
+    for(let imagens of cuentosFinales[v].imagenes){
+         $("#slideshow").append('<div class="contenedoraSlider"><img   alt=" " class="imagenSlider" src="'+  imagens +'  "></div>');
+       n++;
+  }  
+   
+}
